98b29fad3a824fe0b4df8f556ae33649ca61eed0
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-06-05 08:23:44 -0700

    setup server.js and index

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+const projectRouter = require("./data/helpers/projectRouter.js");
+const actionRouter = require("./data/helpers/actionRouter.js");
+
+server.use(express.json());
+
+server.use("/projects", logger, projectRouter);
+server.use("/actions", logger, actionRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Hey I'm working!</h2>`);
+});
+
+function logger(req, res, next) {
+    const today = new Date().toLocaleDateString('en-US');
+    console.log(`${today} ${req.method} ${req.url}`);
+
+    next();
+}
+
+module.exports = server;
